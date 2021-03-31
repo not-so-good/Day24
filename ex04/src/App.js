8e5132b2102a1b2bbb26d311971cf1978828441a
not-so-good <sanjin.suljanovic@gmail.com>
2021-03-31 23:35:24 +0200

    ex03 Local storage - getItem

---
+++ ex04/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class App extends Component {
+  setLocalStorage() {
+    localStorage.setItem("Arena", "Selection Month");
+  }
+  getLocalStorage() {
+    const myLocalStorageData = localStorage.getItem("Arena");
+    return myLocalStorageData;
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.setLocalStorage}>Set Local Storage</button>
+        <button onClick={this.getLocalStorage}>Get Local Storage</button>
+      </div>
+    );
+  }
+}
+export default App;
